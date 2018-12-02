2eb8a8a0ff7aaf5af98166f4a44bfb628b8e9626
pgr301-exam-atumn-2018-100018 <45242255+pgr301-exam-atumn-2018-100018@users.noreply.github.com>
2018-12-02 13:16:05 +0100

    Metrics start

---
+++ app/src/main/java/app/GraphiteMetricsConfig.java
@@ -0,0 +1,34 @@
+package app;
+
+import com.codahale.metrics.*;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class GraphiteMetricsConfig
+{
+    @Bean
+    public MetricRegistry getRegistry()
+    {
+        return new MetricRegistry();
+    }
+
+    @Bean
+    public GraphiteReporter getReporter(MetricRegistry registry)
+    {
+        Graphite graphite = new Graphite(new InetSocketAddress(System.getenv("GRAPHITE_HOST"), 2003));
+        GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+                .prefixedWith(System.getenv("HOSTEDGRAPHITE_APIKEY"))
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .filter(MetricFilter.ALL)
+                .build(graphite);
+        reporter.start(1, TimeUnit.SECONDS);
+        return reporter;
+    }
+}
