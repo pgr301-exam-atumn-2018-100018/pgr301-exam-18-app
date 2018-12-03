c95bf010275f8375ec6862b19a5d6fbed032ee8d
pgr301-exam-atumn-2018-100018 <45242255+pgr301-exam-atumn-2018-100018@users.noreply.github.com>
2018-12-03 15:44:00 +0100

    metrics test

--- app/src/main/java/app/GraphiteMetricsConfig.java
+++ app/src/main/java/app/GraphiteMetricsConfig.java
@@ -5,6 +5,8 @@ import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
@@ -12,23 +14,23 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class GraphiteMetricsConfig
 {
-    @Bean
-    public MetricRegistry getRegistry()
-    {
-        return new MetricRegistry();
-    }
+//    @Bean
+//    public MetricRegistry getRegistry()
+//    {
+//        return new MetricRegistry();
+//    }
 
-    @Bean
-    public GraphiteReporter getReporter(MetricRegistry registry)
-    {
-        Graphite graphite = new Graphite(new InetSocketAddress(System.getenv("GRAPHITE_HOST"), 2003));
-        GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
-                .prefixedWith(System.getenv("HOSTEDGRAPHITE_APIKEY"))
-                .convertRatesTo(TimeUnit.SECONDS)
-                .convertDurationsTo(TimeUnit.MILLISECONDS)
-                .filter(MetricFilter.ALL)
-                .build(graphite);
-        reporter.start(1, TimeUnit.SECONDS);
-        return reporter;
-    }
+//    @Bean
+//    public GraphiteReporter getReporter(MetricRegistry registry)
+//    {
+//        Graphite graphite = new Graphite(new InetSocketAddress(System.getenv("GRAPHITE_HOST"), 2003));
+//        GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+//                .prefixedWith(System.getenv("HOSTEDGRAPHITE_APIKEY"))
+//                .convertRatesTo(TimeUnit.SECONDS)
+//                .convertDurationsTo(TimeUnit.MILLISECONDS)
+//                .filter(MetricFilter.ALL)
+//                .build(graphite);
+//        reporter.start(1, TimeUnit.SECONDS);
+//        return reporter;
+//    }
 }
